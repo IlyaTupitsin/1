bc1a9afef7744fe4f414d5f2872099a4cfd45ec6
Ilya <tupitsin2004@mail.ru>
2023-03-04 17:18:32 +0300

    работа на паре

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,5 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!")
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            StreamReader fin = new StreamReader("C:\\Users\\Пользователь\\Desktop\\urok.xml");
+            int c;
+            while ((c = fin.Read()) != -1)
+            {
+                Console.Write((char)c);
+            }
+            fin.Close();
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
